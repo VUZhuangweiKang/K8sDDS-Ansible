5e178ca3719b9acba5cb493d6dac2f35fc8ea04a
ZhuangweiKang <zkty9@mail.missouri.edu>
2020-11-14 19:12:33 -0600

    update

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for svr in {1..100}
+for svr in {1..10}
 do
     echo "----------- Siemens $svr -----------" 
     ssh siemens$svr sudo apt-get update && sudo apt-get install -y ansible
